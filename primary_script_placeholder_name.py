e89057a9e2a9c0932e765a564d7a8f4a9e20320b
Naat Ambrosino <89532227+TheNatureWarrior@users.noreply.github.com>
2026-02-13 19:33:38 -0500

    Stopping point 2
    This has been sitting around a while -- I will pick this back up soon.

--- primary_script_placeholder_name.py
+++ primary_script_placeholder_name.py
@@ -19,21 +19,36 @@ def discard_files(*args : str) -> None:
 def create_base_file(file_name : str, num_bits : int) -> None:
     if num_bits < 1:
         raise ValueError('Invalid number of bits given, must be positive.')
-    rows = [['id', 'sound']]
-    for i in range(2**num_bits + 1):
-        rows.append([i, MISSING_VALUE])
+    rows = [('id', 'sound', 'notes')]
+    for i in range(2**num_bits):
+        if i ==0:
+            rows.append((i, ""))
+        else:
+            rows.append((i, MISSING_VALUE, MISSING_VALUE))
     with open(proper_path(file_name), 'w') as f:
         writer = csv.writer(f)
         writer.writerows(rows)
 
         
-
-
 def verify_files():
     if not (file_exists('vowels.csv') and file_exists('consonants.csv') and file_exists('runes.csv')):
-        discard_files('vowels.csv', 'consonants.csv', 'runes.csv', 'sounds.csv', 'words.csv')
-        create_base_file('beep', 5)
-
+        discard_files('vowels.csv', 'consonants.csv', 'runes.csv', 'words.csv')
+        for file_name, bits in (('vowels.csv', VOWEL_BITS), ('consonants.csv', CONSONANT_BITs)):
+            create_base_file(file_name, bits)
+    if not file_exists('runes.csv'):
+        with open(proper_path('consonants.csv'), 'r') as consonant_file:
+            with open(proper_path('vowels.csv'), 'r') as vowel_file:
+                consonant_reader = csv.reader(consonant_file)
+                vowel_reader = csv.reader(vowel_file)
+                consonant_rows = list(consonant_reader)[1:]
+                vowel_rows = list(vowel_reader)[1:]
+                rune_rows = [('id', 'sound', 'notes')]
+                for c_row in consonant_rows:
+                    for v_row in vowel_rows:
+                        if c_row[1] != "" and v_row[1] != "":
+                            for vowel_first in (0, 1):
+                                pass #TODO
+                        elif c_row[1] == "" and v_row[1] != "":
 
     
 def main():
